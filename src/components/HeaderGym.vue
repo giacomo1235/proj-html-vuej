ae682b0f20a421b8abba0c708cdc79cef7bd0343
giacomo1235 <giacomo.passariello@gmail.com>
2022-04-01 16:23:20 +0200

    formattato header

--- src/components/HeaderGym.vue
+++ src/components/HeaderGym.vue
@@ -1,15 +1,84 @@
 <template>
   <header>
-      header
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/images/gym_logo_1x.png" alt="gym-logo" />
+      </div>
+      <nav>
+        <ul class="links">
+          <li @mouseleave="setActiveIndex()" @mouseenter="setActiveIndex(index)" v-for="(link, index) in links" :key="index" :class="{active:index == activeIndex}" class="link"><a :href="link.href">{{link.text}}</a></li>
+          <!-- <li class="link">about us</li>
+          <li class="link">facilities</li>
+          <li class="link">membership</li>
+          <li class="link">tetimonials</li>
+          <li class="link">blog</li> -->
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'HeaderGym'
+  name: 'HeaderGym',
+  data () {
+    return {
+      activeIndex: null,
+      links: [
+        {
+          href: '#!',
+          text: 'home'
+        },
+        {
+          href: '#',
+          text: 'about us'
+        },
+        {
+          href: '#',
+          text: 'facilities'
+        },
+        {
+          href: '#',
+          text: 'membership'
+        },
+        {
+          href: '#!',
+          text: 'testimonials'
+        },
+        {
+          href: '#',
+          text: 'blog'
+        }
+      ]
+    }
+  },
+  methods: {
+    setActiveIndex (index) {
+      this.activeIndex = index
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="sass" scoped>
+@import "../assets/styles/style"
+header
+  background-color: $shark
+  padding: 1rem
+  .container
+    @include flex-layout (center, space-between, wrap)
+    width: 90%
+    margin: auto
+    .links
+      list-style: none
+      .link
+        display: inline-block
+        margin: 0 .5rem
+        &.active
+          a
+            color: $chestnut_rose
+        a
+          text-decoration: none
+          color: $white
+          text-transform: uppercase
 </style>
